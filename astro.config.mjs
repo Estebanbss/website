7a4e7687866bbb7a6affae7e3f1b7face071ec58
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-16 17:54:33 -0500

    Update i18n default language to Spanish, refactor middleware, and improve UI layout and performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,4 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  i18n: {
-     defaultLocale: "es",
-     locales: ["es", "en", "fr"],
-     routing: "manual"
-   }
 });
