0f8a55c67b4d7616cfb1f063e46ac4c0e201f45a
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-16 15:05:56 -0500

    Refactor middleware to redirect to default path based on domain

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,14 @@ const INDEX_PATH = "/";
 const DEFAULT_PATH = "/es";
 
 export const onRequest = defineMiddleware((context, next) => {
+     let domain = context.url.hostname;
 
-  if (context.url.pathname === INDEX_PATH) {
-
+  // Verifica si el dominio es localhost
+  if (domain === "localhost") {
+    // Redirige a la ruta predeterminada si el dominio es localhost
     return Response.redirect(new URL(DEFAULT_PATH, context.url), 302);
-
   }
 
-  return next();
+  // Redirige a la ruta predeterminada si no es localhost
+  return Response.redirect(new URL(DEFAULT_PATH, `https://${domain}`), 302);
 });
