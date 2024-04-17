45ee2606d54ccf96a745afc8e37af0ea52963007
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-17 12:21:32 -0500

    Fixing min height of the header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), playformCompress()
-]
+  integrations: [tailwind(), icon(), playformCompress()],
+  optimizeCss: {
+    link: ({ rel, href }) => {
+      // Verifica si el atributo "rel" ya contiene "stylesheet"
+      if (rel.includes('stylesheet')) {
+        // Añade el atributo "media" con el valor "all"
+        return `<link rel="${rel}" href="${href}" media="all" />`;
+      }
+      // Si no contiene "stylesheet", devuelve el enlace sin modificar
+      return `<link rel="${rel}" href="${href}" />`;
+    }
+  }
 });
