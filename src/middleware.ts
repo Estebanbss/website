4241e4707b34be71fd08db657db16cec2052af9f
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-18 00:53:23 -0500

    Fixing Google TagManager script loading issue and improving heading styles

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,49 +2,20 @@ import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
      let domain = context.url.pathname;
-     
-     let pages = ["/ln", "/gh", "/git", "/cv", "/blog", "/contact"];
+     const redirects: { [key: string]: string } = {
+          "/ln": "https://www.linkedin.com/in/estebanbss/",
+          "/gh": "https://github.com/Estebanbss",
+          "/git": "https://github.com/Estebanbss/website",
+          "/cv": "https://drive.google.com/file/d/1ypkmvNxf4ZNEo4kBh01O38uD539V-2ca/view?usp=sharing",
+          "/en/cv": "https://drive.google.com/file/d/1W1c5wMeKC0w7r-W-xoKyuJPySc2guPjy/view?usp=sharing"
+        };
+     let pages = ["/ln", "/gh", "/git", "/cv", "/en/cv", "/", "/en", "/es", "/fr"];
      let i = 0;
 
-     if (domain ===  "/ln") {
-          return Response.redirect(
-               new URL("https://www.linkedin.com/in/estebanbss/", context.url),
-               302
-          );
-     }
+     const redirectUrl = redirects[domain] || null;
 
-     if (domain ===  "/gh") {
-          return Response.redirect(
-               new URL("https://github.com/Estebanbss", context.url),
-               302
-          );
-     }
-
-     if (domain ===  "/git") {
-          return Response.redirect(
-               new URL("https://github.com/Estebanbss/website", context.url),
-               302
-          );
-     }
-
-     if (domain ===  "/en/cv") {
-          return Response.redirect(
-               new URL(
-                    "https://drive.google.com/file/d/1W1c5wMeKC0w7r-W-xoKyuJPySc2guPjy/view?usp=sharing",
-                    context.url
-               ),
-               302
-          );
-     }
-
-     if (domain ===  "/cv") {
-          return Response.redirect(
-               new URL(
-                    "https://drive.google.com/file/d/1ypkmvNxf4ZNEo4kBh01O38uD539V-2ca/view?usp=sharing",
-                    context.url
-               ),
-               302
-          );
+     if (redirectUrl) {
+          return Response.redirect(new URL(redirectUrl, context.url), 302);
      }
 
      pages.forEach((page) => {
@@ -55,7 +26,7 @@ export const onRequest = defineMiddleware((context, next) => {
           }
      });
 
-     if(i === pages.length){
+     if(i > 0){
           return next();
      }else{
           return Response.redirect(
@@ -63,6 +34,6 @@ export const onRequest = defineMiddleware((context, next) => {
                302
           );
      }
-
-     return next();
 });
+
+
