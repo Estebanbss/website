93726493edce1d861e6ecc30fd12ddce4ee481ba
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-17 22:24:54 -0500

    ⚡Adding redirect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,55 @@ import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
      let domain = context.url.href;
-     let webDomain = 'https://estebanbss.dev';
+     let webDomain = "https://estebanbss.dev";
+     let pages = ["/ln", "/gh", "/git", "/cv", "/blog", "/contact"];
 
-  if (domain === webDomain + '/ln' ) {
-    return Response.redirect(new URL('https://www.linkedin.com/in/estebanbss/', context.url), 302);
-  }
+     if (domain === webDomain + "/ln") {
+          return Response.redirect(
+               new URL("https://www.linkedin.com/in/estebanbss/", context.url),
+               302
+          );
+     }
 
-  if(domain === webDomain + '/gh') {
-     return Response.redirect(new URL('https://github.com/Estebanbss', context.url), 302);
-  }
+     if (domain === webDomain + "/gh") {
+          return Response.redirect(
+               new URL("https://github.com/Estebanbss", context.url),
+               302
+          );
+     }
 
-  if(domain === webDomain + '/git') {
-     return Response.redirect(new URL('https://github.com/Estebanbss/website', context.url), 302);
-  }     
-  
-  return next();
-});                                               
+     if (domain === webDomain + "/git") {
+          return Response.redirect(
+               new URL("https://github.com/Estebanbss/website", context.url),
+               302
+          );
+     }
+
+     if (domain === webDomain + "/en/cv") {
+          return Response.redirect(
+               new URL(
+                    "https://drive.google.com/file/d/1W1c5wMeKC0w7r-W-xoKyuJPySc2guPjy/view?usp=sharing",
+                    context.url
+               ),
+               302
+          );
+     }
+
+     if (domain === webDomain + "/cv") {
+          return Response.redirect(
+               new URL(
+                    "https://drive.google.com/file/d/1ypkmvNxf4ZNEo4kBh01O38uD539V-2ca/view?usp=sharing",
+                    context.url
+               ),
+               302
+          );
+     }
+
+     pages.forEach((page) => {
+          if (domain !== webDomain + page) {
+               return Response.redirect(new URL(webDomain, context.url), 302);
+          }
+     } );
+
+     return next();
+});
