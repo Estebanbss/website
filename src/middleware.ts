0bfab96f91e663cd49d976c623385ad96af3a47c
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-18 10:56:26 -0500

    ⭐ Fixing Google TagManager script loading issue and improving heading styles

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,29 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-     let domain = context.url.pathname;
-     const redirects: { [key: string]: string } = {
-          "/ln": "https://www.linkedin.com/in/estebanbss/",
-          "/gh": "https://github.com/Estebanbss",
-          "/git": "https://github.com/Estebanbss/website",
-          "/cv": "https://drive.google.com/file/d/1ypkmvNxf4ZNEo4kBh01O38uD539V-2ca/view?usp=sharing",
-          "/en/cv": "https://drive.google.com/file/d/1W1c5wMeKC0w7r-W-xoKyuJPySc2guPjy/view?usp=sharing"
-        };
-     let pages = ["/ln", "/gh", "/git", "/cv", "/en/cv", "/", "/en", "/es", "/fr"];
-     let i = 0;
+    let domain = context.url.pathname;
+    const redirects: { [key: string]: string } = {
+        "/ln": "https://www.linkedin.com/in/estebanbss/",
+        "/gh": "https://github.com/Estebanbss",
+        "/git": "https://github.com/Estebanbss/website",
+        "/cv": "https://drive.google.com/file/d/1ypkmvNxf4ZNEo4kBh01O38uD539V-2ca/view?usp=sharing",
+        "/en/cv": "https://drive.google.com/file/d/1W1c5wMeKC0w7r-W-xoKyuJPySc2guPjy/view?usp=sharing"
+    };
 
-     const redirectUrl = redirects[domain] || null;
+    // Verificar si la ruta es una ruta de i18n
+    const isI18nRoute = domain.startsWith("/en/") || domain.startsWith("/es/") || domain.startsWith("/fr/");
 
-     if (redirectUrl) {
-          return Response.redirect(new URL(redirectUrl, context.url), 302);
-     }
+    // Si la ruta es una ruta de i18n, pasar al siguiente middleware
+    if (isI18nRoute) {
+        return next();
+    }
 
-     pages.forEach((page) => {
-          if (domain ===  page || domain === '/') {
-               i++;
-          }else{
-               return;
-          }
-     });
+    // Realizar redirección si la ruta está en la lista de redirecciones
+    const redirectUrl = redirects[domain] || null;
+    if (redirectUrl) {
+        return Response.redirect(new URL(redirectUrl, context.url), 302);
+    }
 
-     if(i > 0){
-          return next();
-     }else{
-          return Response.redirect(
-               new URL(context.url.origin, context.url),
-               302
-          );
-     }
+    // Permitir que el sistema de enrutamiento maneje la solicitud
+    return next();
 });
-
-
