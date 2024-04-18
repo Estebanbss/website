44c636103229d35771fdc9a080b1f8b140059334
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-17 21:29:31 -0500

    ⚙️ Fixing middlewae url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-
-  if (context.url.pathname === '/ln') {
+     let domain = context.url.hostname;
+     let webDomain = 'https://estebanbss.com';
+     
+  if (domain === webDomain + '/ln' ) {
     return Response.redirect(new URL('https://www.linkedin.com/in/estebanbss/', context.url), 302);
   }
 
-  if(context.url.pathname === '/gh') {
+  if(domain === webDomain + '/gh') {
      return Response.redirect(new URL('https://github.com/Estebanbss', context.url), 302);
   }
 
-  if(context.url.pathname === '/git') {
+  if(domain === webDomain + '/git') {
      return Response.redirect(new URL('https://github.com/Estebanbss/website', context.url), 302);
   }     
   
