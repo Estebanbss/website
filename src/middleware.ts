54398eea3f78ffdc560e1ab0beb509f266e035b3
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-17 23:00:36 -0500

    ☀️ Improving routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,33 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-     let domain = context.url.href;
-     let webDomain = "https://estebanbss.dev";
+     let domain = context.url.pathname;
+     
      let pages = ["/ln", "/gh", "/git", "/cv", "/blog", "/contact"];
+     let i = 0;
 
-     if (domain === webDomain + "/ln") {
+     if (domain ===  "/ln") {
           return Response.redirect(
                new URL("https://www.linkedin.com/in/estebanbss/", context.url),
                302
           );
      }
 
-     if (domain === webDomain + "/gh") {
+     if (domain ===  "/gh") {
           return Response.redirect(
                new URL("https://github.com/Estebanbss", context.url),
                302
           );
      }
 
-     if (domain === webDomain + "/git") {
+     if (domain ===  "/git") {
           return Response.redirect(
                new URL("https://github.com/Estebanbss/website", context.url),
                302
           );
      }
 
-     if (domain === webDomain + "/en/cv") {
+     if (domain ===  "/en/cv") {
           return Response.redirect(
                new URL(
                     "https://drive.google.com/file/d/1W1c5wMeKC0w7r-W-xoKyuJPySc2guPjy/view?usp=sharing",
@@ -36,7 +37,7 @@ export const onRequest = defineMiddleware((context, next) => {
           );
      }
 
-     if (domain === webDomain + "/cv") {
+     if (domain ===  "/cv") {
           return Response.redirect(
                new URL(
                     "https://drive.google.com/file/d/1ypkmvNxf4ZNEo4kBh01O38uD539V-2ca/view?usp=sharing",
@@ -47,10 +48,21 @@ export const onRequest = defineMiddleware((context, next) => {
      }
 
      pages.forEach((page) => {
-          if (domain !== webDomain + page) {
-               return Response.redirect(new URL(webDomain, context.url), 302);
+          if (domain ===  page || domain === '/') {
+               i++;
+          }else{
+               return;
           }
-     } );
+     });
+
+     if(i === pages.length){
+          return next();
+     }else{
+          return Response.redirect(
+               new URL(context.url.origin, context.url),
+               302
+          );
+     }
 
      return next();
 });
