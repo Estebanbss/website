9b5f976296005eb2c9f8dd72fccfdacbd6e26aad
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-04-19 01:22:22 -0500

    Setting all

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,6 @@ import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://estebanbss.dev",
   integrations: [tailwind(), icon(), playformCompress()],
-  optimizeCss: {
-    link: ({ rel, href }) => {
-      // Verifica si el atributo "rel" ya contiene "stylesheet"
-      if (rel.includes('stylesheet')) {
-        // Añade el atributo "media" con el valor "all"
-        return `<link rel="${rel}" href="${href}" media="all" />`;
-      }
-      // Si no contiene "stylesheet", devuelve el enlace sin modificar
-      return `<link rel="${rel}" href="${href}" />`;
-    }
-  },
 });
